205e3f9b044eb4c387608db94c9879435f3b33aa
Wagner Rodrigues <wagnersr92@gmail.com>
2023-05-05 21:06:09 -0300

    Criando update e lidando com uuid

--- core/crud.ts
+++ core/crud.ts
@@ -1,17 +1,22 @@
 //const fs = require("fs"); CommonJS
+import { UUID } from "crypto";
 import fs from "fs"
+import { v4 as uuid } from 'uuid';
+
 const DB_FILE_PATH = "./core/db"
 
 console.log("[CRUD]")
 
 interface Todo {
+    id: string;
     date: string;
     content: string;
     done: boolean;
 }
 
-function create(content: string){
+function create(content: string): Todo{
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false,
@@ -26,7 +31,7 @@ function create(content: string){
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
         todos
     }, null, 2))
-    return content;
+    return todo;
 }
 
 function read(): Array<Todo>{
@@ -39,6 +44,32 @@ function read(): Array<Todo>{
     return db.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>): Todo{
+    let updatedTodo;
+    const todos = read();
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if(isToUpdate) {
+            updatedTodo = Object.assign(currentTodo, partialTodo);
+        }
+    })
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
+
+    if(!updatedTodo){
+        throw new Error("Please, provide another ID!")
+    }
+
+    return updatedTodo;
+}
+
+function updateContentById(id: string, content: string): Todo {
+    return update(id, {
+        content,
+    })
+}
+
 function CLEAR_DB(){
     fs.writeFileSync(DB_FILE_PATH, "");
 }
@@ -46,5 +77,11 @@ function CLEAR_DB(){
 //SIMULATION
 CLEAR_DB()
 create("Primeira TODO");
-create("Segunda TODO");
+create("Primeira TODO");
+const terceiraTodo = create("Segunda TODO");
+// update(terceiraTodo.id, {
+//     content: "Atualizada!",
+//     done: true 
+// });
+updateContentById(terceiraTodo.id, "Teste!");
 console.log(read());
