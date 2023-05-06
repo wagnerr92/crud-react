8dae2f970f66498f9fa8eadaffaf40256ecf854f
Wagner Rodrigues <wagnersr92@gmail.com>
2023-05-05 21:34:41 -0300

    DELETE

--- core/crud.ts
+++ core/crud.ts
@@ -1,5 +1,4 @@
 //const fs = require("fs"); CommonJS
-import { UUID } from "crypto";
 import fs from "fs"
 import { v4 as uuid } from 'uuid';
 
@@ -7,8 +6,10 @@ const DB_FILE_PATH = "./core/db"
 
 console.log("[CRUD]")
 
+type UUID = string;
+
 interface Todo {
-    id: string;
+    id: UUID;
     date: string;
     content: string;
     done: boolean;
@@ -44,7 +45,7 @@ function read(): Array<Todo>{
     return db.todos;
 }
 
-function update(id: string, partialTodo: Partial<Todo>): Todo{
+function update(id: UUID, partialTodo: Partial<Todo>): Todo{
     let updatedTodo;
     const todos = read();
     todos.forEach((currentTodo) => {
@@ -64,13 +65,13 @@ function update(id: string, partialTodo: Partial<Todo>): Todo{
     return updatedTodo;
 }
 
-function updateContentById(id: string, content: string): Todo {
+function updateContentById(id: UUID, content: string): Todo {
     return update(id, {
         content,
     })
 }
 
-function deleteById(id: string){
+function deleteById(id: UUID){
     const todos = read()
 
     const todosWithoutOne = todos.filter((todo) => {
